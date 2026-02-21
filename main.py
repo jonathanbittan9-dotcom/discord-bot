4f3bd4a70d2d6538c70a5e71dd11b74c18fb1642
jonathanbittan9-dotcom <jonathan.bittan9@gmail.com>
2026-02-21 13:54:40 +0200

    bot

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import os
 from asyncio import wait_for
-from email.mime import message
 import random
-
+import json
 import discord
 from discord.ext import commands
-from discord import Intents, app_commands
+from discord import Intents, Interaction, app_commands
+try:
+    with open("xp.json", "r") as f:
+        user_xp = json.load(f)
+except FileNotFoundError:
+    user_id = str(interaction.user.id)
+    if user_id not in user_xp:
+        user_xp[user_id] = 0
+
 
 
 class Client(commands.Bot):
     async def on_ready(self):
         await self.tree.sync(guild=GUILD_ID)
         print(f'logged on as {self.user}!')
+        
 
     
 
@@ -65,6 +73,67 @@ async def calculator_command(interaction: discord.Interaction, num1: int, operat
             await interaction.response.send_message("you cant divide by 0")
         else:
             await interaction.response.send_message(num1/num2)
+@Bot.tree.command(name="gamble" , description="gamble and earn suprises!", guild=GUILD_ID)
+async def gamble_command(interaction: discord.Interaction):
+   
+    await interaction.response.send_message("With what do you want to gamble? choose from: dice, coin, number")
+    def check(message):
+        return message.author == interaction.user and message.channel == interaction.channel
+    try:
+        msg= await Bot.wait_for('message' , check=check, timeout=10)
+    except:
+        await interaction.followup.send("you took too long to respond")
+        return
+    user_id = interaction.user.id
+    if user_id not in user_xp:
+        user_xp[user_id] = 0
+
+    if msg.content=="dice":
+        await interaction.followup.send(f'you rolled a dice and got: {random.randint(1,6)}')
+    elif msg.content=="coin":
+        await interaction.followup.send(f'you flipped up a coin and got:{random.choice(["heads","tails"])}')
+    elif msg.content == "number":
+        number= random.randint(1,1000)
+        await interaction.followup.send(f'you recived the number:{number}')
+        if number == 666:
+            await interaction.followup.send("you recived the devil number!")
+            await interaction.followup.send("by the way, you earned 30 xp!")
+            user_xp[user_id] += 30
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+        elif number == 667:
+            await interaction.followup.send("you recieved 67777777!!!!!!!")
+            await interaction.followup.send("you earned 100 xp!")
+            user_xp[user_id] += 100
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif number == 67:
+            await interaction.followup.send("you recieved the regular 6777777!!!!!!!!!!")
+            await interaction.followup.send("you earned 120 xp!")
+            user_xp[user_id] += 120
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif number == 777:
+            await interaction.followup.send("you recieved the lucky number!")
+            await interaction.followup.send("you earned 50 xp!")
+            user_xp[user_id] += 50
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif number == 900:
+            await interaction.followup.send("you recieved the unlucky number!")
+            await interaction.followup.send("you lost 50 xp!")
+            user_xp[user_id] -= 50
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+       
+    
+    
+
 
 token = os.getenv("DISCORD_BOT_TOKEN")
 
