c24d38ab2c6d3c14eae86cd6ff5b02f19593d3cb
jonathanbittan9-dotcom <jonathan.bittan9@gmail.com>
2026-02-25 18:59:58 +0200

    commit

--- main.py
+++ main.py
@@ -8,24 +8,55 @@ from discord import Intents, Interaction, app_commands
 try:
     with open("xp.json", "r") as f:
         user_xp = json.load(f)
+
 except FileNotFoundError:
     user_id = str(interaction.user.id)
     if user_id not in user_xp:
         user_xp[user_id] = 0
-
-
+   
 
 class Client(commands.Bot):
     async def on_ready(self):
         await self.tree.sync(guild=GUILD_ID)
         print(f'logged on as {self.user}!')
-        
+       
 
     
 
     async def on_message(self, message):
         if message.author == self.user:
             return
+        msg_content = message.content.lower()
+        warn_words = [
+        "fuck", "shit", "damn", "bitch" ,"ass" , "hell" , "crap" , "bastard" , "piss" , "whore" ,
+        "slut" ,"cunt" , "retard" , "retarded" ,"fag" , "faggot" , "nigger" , "nigga" , "tranny" , "cancer"
+        "die" , "died",
+        "chink" ,"spic" , "kike" , "wetback" , "discord.gg" , "http://" , "https://" , "www." ,"bit.ly",
+        "tinyurl" , ".com" , ".net" , ".org" , "kys" , "kill yourself" , "neck yourself" , "loser" , "idiot" ,
+        "moron" , "stupid" , "dumb" , "ugly" , "fat" , "worthless" ,"pathetic" , "porn" , "sex" , "nude" , "naked",
+        "nsfw" , "xxx" , "pornhub" , "xvideos" , "redtube" , "youporn" , "brazzers" , "bangbros" , "evilangel" , "digitalplayground",
+        "explicit" , "@everyone" , "@here" , "black monkey"
+        ]
+
+        instant_ban_words = [
+        "nigger" , "nigga" , "chink" , "spic" , "kike" , "wetback", "black monkey", "nazi",
+        "i will kill you" , "i am going to kill you" , "gonna kill you" , "your address is" , "your phone number is",
+        "i know where you live" , "i am coming for you" , "you better watch out" , "i will find you" , 
+        "child porn" , "cp" , "loli" , "shota" , "rape" , "sexual assualt" , "kill yourself" , 
+        "kys" , "hang yourself" , "neck yourself" , "commit suidice" , "drugs" , "i want drugs", 
+        "make drugs" , "alcohol" , "make alcohol", "drunk" , "your real name is" , "you live at" , "your parents are",
+        "you ip address is" , "you email address is" , 
+        ]
+
+        # if any(bad_word in msg_content for bad_word in warn_words):
+        #     await message.delete()
+        #     await message.channel.send(f"{message.author.mention} , you got a warn! its warn number {Warning_user[message.author.id]}! if you will get to 5 warns, you will be ban from the server!")
+        #     Warning_user[message.author.id] += 1
+        #     with open("warnings.json" , "w") as f:
+        #         json.dump(Warning_user, f)
+        #     if Warning_user == 5:
+        #     #remember to generate the ban here
+
         
 intents=discord.Intents.default()
 intents.message_content=True
@@ -89,9 +120,64 @@ async def gamble_command(interaction: discord.Interaction):
         user_xp[user_id] = 0
 
     if msg.content=="dice":
-        await interaction.followup.send(f'you rolled a dice and got: {random.randint(1,6)}')
+        await interaction.followup.send(f'alright! , you will roll to dives, and if the numbers are the same you will get xp!')
+        dice1= random.randint(1,6)
+        dice2= random.randint(1,6)
+        if dice1 == 1 and dice2 ==1:
+            await interaction.followup.send(f'you rolled a double 1! you earned 10 xp!')
+            user_xp[user_id] += 10
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif dice1 == 2 and dice2 == 2:
+            await interaction.followup.send(f'you rolled a double 2! you earned 20 xp!')
+            user_xp[user_id] += 20
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif dice1 == 3 and dice2 == 3:
+            await interaction.followup.send(f'you rolled a double 3! you earned 30 xp!')
+            user_xp[user_id] += 30
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif dice1 == 4 and dice2 == 4:
+            await interaction.followup.send(f'you rolled a double 4! you earned 40 xp!')
+            user_xp[user_id] += 40
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif dice1 == 5 and dice2 == 5:
+            await interaction.followup.send(f'you rolled a double 5! you earned 50 xp!')
+            user_xp[user_id] += 50
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif dice1 == 6 and dice2 == 6:
+            await interaction.followup.send(f'you rolled a double 6! you earned 60 xp!')
+            user_xp[user_id] += 60
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        else:
+            await interaction.followup.send(f'you rolled a {dice1} and a {dice2} , you didnt earn any xp!')
+
     elif msg.content=="coin":
-        await interaction.followup.send(f'you flipped up a coin and got:{random.choice(["heads","tails"])}')
+        gamble= random.choice(["heads","tails"])
+        await interaction.followup.send(f'you flipped up a coin and got:{gamble}')
+        if gamble == "heads":
+            await interaction.followup.send("you earned 40 xp!")
+            user_xp[user_id] += 40
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif gamble == "tails":
+            await interaction.followup.send("you lost 20 xp!")
+            user_xp[user_id] -= 20
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+
     elif msg.content == "number":
         number= random.randint(1,1000)
         await interaction.followup.send(f'you recived the number:{number}')
@@ -130,10 +216,41 @@ async def gamble_command(interaction: discord.Interaction):
             with open("xp.json", "w") as f:
                 json.dump(user_xp, f)
             await interaction.followup.send(f'you have {user_xp[user_id]} xp')
-       
-    
-    
-
+        elif number == 1000:
+            await interaction.followup.send("you recieved the max number!")
+            await interaction.followup.send("you earned 200 xp!")
+            user_xp[user_id] += 200
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif number == 1:
+            await interaction.followup.send("you recieved the min number!")
+            await interaction.followup.send("you lost 20 xp!")
+            user_xp[user_id] -= 20
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        elif number == 500:
+            await interaction.followup.send("you recieved the middle number!")
+            await interaction.followup.send("you earned 20 xp!")
+            user_xp[user_id] += 20
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+        elif number == 250:
+            await interaction.followup.send("you recieved the quarter number!")
+            await interaction.followup.send("you earned 10 xp!")
+            user_xp[user_id] += 10
+            with open("xp.json", "w") as f:
+                json.dump(user_xp, f)
+            await interaction.followup.send(f'you have {user_xp[user_id]} xp')
+        else:
+            await interaction.followup.send("you didnt recive any special number, you didnt earn or lose any xp!")
+@Bot.tree.command(name="xp" , description="check your xp", guild=GUILD_ID)
+async def xp_command(interaction: discord.Interaction):
+    user_id = interaction.user.id
+    if user_id not in user_xp:
+        user_xp[user_id] = 0
+    await interaction.response.send_message(f'you have {user_xp[user_id]} xp')
 
 token = os.getenv("DISCORD_BOT_TOKEN")
 
